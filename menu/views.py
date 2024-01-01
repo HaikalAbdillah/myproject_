a66be40e549da2562683e515c4c7cdba1b05ee0e
Haikal Abdillah <haikalabdillah62@gmail.com>
2024-01-01 17:53:46 +0700

    Mengubah file

--- menu/views.py
+++ menu/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from menu.models import Produk
+
+def produk(request):
+    produks = Produk.objects.all()
+    context={
+        'produks': produks,
+    }
+    produk = loader.get_template('produk.html')
+    return HttpResponse(produk.render(context,request))
+
 
 def home(request):
-    return render(request, 'home.html')
+    home = loader.get_template('home.html')
+    return HttpResponse(home.render())
